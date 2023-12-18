2224983f0b523fd97135275ba212fdd3231b8eae
Iván <ivangonzalezgonzalez09@gmail.com>
2023-12-18 13:07:19 +0100

    18/12/2023 - 13:07

---
+++ rgb-hsv/jad/4_realtime_color_detection_scrolls.py
@@ -0,0 +1,52 @@
+import numpy as np
+import cv2
+from PIL import Image 
+
+# Trackbar callback
+def on_trackbar(val):
+    # Extract values of trackbars
+    hue_min = cv2.getTrackbarPos("Hue Min", "TrackedBars")
+    hue_max = cv2.getTrackbarPos("Hue Max", "TrackedBars")
+    sat_min = cv2.getTrackbarPos("Sat Min", "TrackedBars")
+    sat_max = cv2.getTrackbarPos("Sat Max", "TrackedBars")
+    int_min = cv2.getTrackbarPos("Int Min", "TrackedBars")
+    int_max = cv2.getTrackbarPos("Int Max", "TrackedBars")
+
+    # Define range of color in HSV
+    lower_col = np.array([hue_min, sat_min, int_min])
+    upper_col = np.array([hue_max, sat_max, int_max])
+
+    # Threshold the HSV image using inRange function to get only specific colors
+    mask = cv2.inRange(hsv, lower_col, upper_col)
+
+    # Transform the mask to BGR to plot with colors
+    mask = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
+
+    # Stack the images to display
+    numpy_horizontal = np.hstack((frame, mask))
+
+    # Display the resulting frame
+    cv2.imshow("TrackedBars", numpy_horizontal)
+
+# Load an image
+image_path = "/home/ivan/Descargas/mustache_project/rgb-hsv/output_image.tiff"
+frame = cv2.imread(image_path)
+
+# Create trackbars to change the value of hue min and max and sat min
+cv2.namedWindow("TrackedBars")
+cv2.createTrackbar("Hue Min", "TrackedBars", 0, 179, on_trackbar)
+cv2.createTrackbar("Hue Max", "TrackedBars", 0, 179, on_trackbar)
+cv2.createTrackbar("Sat Min", "TrackedBars", 0, 255, on_trackbar)
+cv2.createTrackbar("Sat Max", "TrackedBars", 0, 255, on_trackbar)
+cv2.createTrackbar("Int Min", "TrackedBars", 0, 255, on_trackbar)
+cv2.createTrackbar("Int Max", "TrackedBars", 0, 255, on_trackbar)
+
+# Converting the image to HSV
+hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+# Trackbar callback
+on_trackbar(0)
+
+# Wait for a key event and then close the window
+cv2.waitKey(0)
+cv2.destroyAllWindows()
--- rgb-hsv/jad/max_min.py
+++ rgb-hsv/jad/max_min.py
@@ -10,7 +10,16 @@ import os
 r2 = 184
 
 # Separated CSV file paths (hayat data)
+# Separated CSV file paths (hayat data)
 data_path = "/media/ivan/Ivan/my_data/class_n_0.0.csv"     # 93818 x 553   #0  # dirty CB                                   # 45000
+#data_path = "/media/ivan/Ivan/my_data/class_n_1.0.csv"     # 27084 x 553   #1  # Shadow CB                                 # 15000
+#                                                                           #2  # Missclassified CB   
+#data_path = "/media/ivan/Ivan/my_data/class_n_3.0.csv"     # 44411 x 553   #3  # Meat                                      # 20000
+#data_path = "/media/ivan/Ivan/my_data/class_n_4.0.csv"     # 17398 x 553   #4  # Meat Shadow                               # 8500
+#data_path = "/media/ivan/Ivan/my_data/class_n_5.0.csv"     # 176 x 553     #5  # Missclassified meat                       # 80
+#data_path = "/media/ivan/Ivan/my_data/class_n_6.0.csv"     # 2569 x 553    #6  # Fat                                       # 1250
+#data_path = "/media/ivan/Ivan/my_data/class_n_7.0.csv"     # 10643 x 553   #7  # Fat Shadow                                # 5000
+#data_path = "/media/ivan/Ivan/my_data/class_n_8.0.csv"     # 26436 x 553   #8  # Missclassified fat                        # 13000
 #data_path = "/media/ivan/Ivan/my_data/class_n_9.0.csv"     # 152987 x 553  #9  # PEHD Red Plastic                          # 75000
 #data_path = "/media/ivan/Ivan/my_data/class_n_10.0.csv"    # 9476 x 553    #10  # PEHD small pieces Red plastic on PORK    # 4500
 
@@ -39,66 +48,32 @@ index_80 = 80
 index_44 = 44
 index_39 = 39
 
+# all points of interest
+points_list = [25,30,39,44,53,57,61,80,112,146,154,159]
+
 n_total_samples = np.shape(data_array)[0]
 num_samples_to_select = 20
 random_samples_indexes = random.sample(range(n_total_samples), num_samples_to_select)
-random_samples = data_array[random_samples_indexes, :]
-
-# plotting
-'''
-plt.figure(figsize=(16, 12))
-
-for i, sample_signal in enumerate(random_samples):
-    plt.plot(sample_signal, label=f'Sample n.{i + 1}')
-
-plt.title('20 random signal samples with mean and std of points of interest')
-plt.xlabel('x')
-plt.ylabel('y')
-plt.legend()
-plt.show()
-'''
+random_samples = spectral[random_samples_indexes, :]
 
 # ====================================================================================================
 
-medias_x = []
-stds_x = []
-medias_y = []
-stds_y = []
+means = [np.mean(df.iloc[:, point]) for point in points_list]
+stds = [np.std(df.iloc[:, point]) for point in points_list]
 
-points_list = [25,30,39,44,53,57,61,80,112,146,154,159]
+# Plot random samples
+for i, sample_signal in enumerate(random_samples):
+    plt.plot(sample_signal, label=f'Sample n.{i + 1}', alpha=0.5)
 
-for point in points_list:
-    samples_in_point = df[(df['x'] == point[0]) & (df['y'] == point[1])]
-    medias_x.append(samples_in_point['x'].mean())
-    stds_x.append(samples_in_point['x'].std())
-    medias_y.append(samples_in_point['y'].mean())
-    stds_y.append(samples_in_point['y'].std())
+for i, point in enumerate(points_list):
+    plt.scatter(point, means[i], color='red')
+    plt.errorbar(point, means[i], yerr=stds[i], color='black', linestyle='None', marker='o')
+    plt.text(point, means[i]-stds[i]-0.01, f'{point}', fontsize=8, ha='center', va='center')
+    plt.text(point-0.1, means[i]+stds[i]+0.01, f'{stds[i]:.2f}', fontsize=10, ha='right', va='center', color='blue')
+    plt.text(point+0.1, means[i]+stds[i]+0.01, f'{means[i]:.2f}', fontsize=10, ha='left', va='center', color='red')
 
-# Convertir a numpy array para facilitar el manejo
-medias_x = np.array(medias_x)
-stds_x = np.array(stds_x)
-medias_y = np.array(medias_y)
-stds_y = np.array(stds_y)
-
-# Tu gráfico original con las 20 muestras aleatorias
-plt.scatter(df['x'], df['y'], label='Dataset Completo', alpha=0.5)
-
-# Plotear cada punto del listado con la media y la desviación estándar
-for i, punto in enumerate(points_list):
-    plt.scatter(punto[0], punto[1], color='red', marker='x', label=f'Punto de Interés {i + 1}')
-    plt.errorbar(punto[0], punto[1], xerr=stds_x[i], yerr=stds_y[i], color='red', linestyle='None', label='Desviación Estándar')
-
-# Añadir etiquetas y leyenda
-plt.xlabel('Coordenada X')
-plt.ylabel('Coordenada Y')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('Std | Mean')
 plt.legend()
-plt.title('Dataset Completo con Media y Desviación Estándar en Puntos de Interés')
-
-# Mostrar el gráfico
 plt.show()
-
-
-
-
-
-
