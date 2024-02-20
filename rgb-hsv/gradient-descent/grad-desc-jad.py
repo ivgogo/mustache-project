95a2a8e798232492bb9806b10aff936bd0717413
Iván <ivangonzalezgonzalez09@gmail.com>
2024-02-20 13:03:31 +0100

    20/02/2024 - 13:03

--- rgb-hsv/gradient-descent/grad-desc-jad.py
+++ rgb-hsv/gradient-descent/grad-desc-jad.py
@@ -6,13 +6,11 @@ import pandas as pd
 dataframe = pd.read_csv("/home/ivan/Descargas/csv_test.csv")
 data_array = np.array(dataframe)
 
-# =============================
-start = np.array([0, 0])
-
 # https://pub.towardsai.net/gradient-descent-algorithm-explained-2fe9da0de9a2
 def gradient_descent(data_array, lr):
     
-    y1, y2 = 0
+    y1 = 0
+    y2 = 0
     m = len(data_array)
     
     for i in range(len(data_array)):
@@ -21,9 +19,13 @@ def gradient_descent(data_array, lr):
         value25, value80, value154 = data_array[i]
 
         # Operation 
-        y1 = y1 - lr*((1/2*m)*())
-        y2 = y2 - lr*()
+        y1 = y1 - lr*((1/2*m)*(2*value154*y1-2*value154*value25))
+        y2 = y2 - lr*((1/2*m)*(2*value154*y2-2*value154*value80))
 
 
     parameters = [y1, y2]
     return parameters
+
+# start = np.array([0, 0])
+g = gradient_descent(data_array, lr=0.1)
+print(f'Result: {g}')
